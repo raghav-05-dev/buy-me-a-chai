1fe14c823ee4f1386e9888370e07ab30bcfa02e5
raghav-05-dev <raghavp884@gmail.com>
2026-07-09 23:23:30 +0530

    minor chnages

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Buy me a Chai - Fund your projects with chai.",
+  title: "Buy me a Chai - Fund your projects with a sip of chai.",
   description: "This website is a crownfunding platform for creators.",
 };
 
@@ -22,7 +24,13 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <div className="min-h-screen">
+        {children}
+        </div>
+        <Footer />
+        </body>
     </html>
   );
 }
---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Buy me a Chai - Fund your projects with a sip of chai.</p>
+    </footer>
+  )
+}
+
+export default Footer
