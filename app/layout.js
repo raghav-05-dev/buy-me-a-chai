e0c01e25af804b8d56f0723789427ce65d73a978
raghav-05-dev <raghavp884@gmail.com>
2026-07-10 20:09:37 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -21,13 +21,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col">
+      <body>
         <Navbar />
-        
-        <main className="flex-1">
+        <div className="min-h-screen">
           {children}
-        </main>
-
+        </div>
         <Footer />
       </body>
     </html>
