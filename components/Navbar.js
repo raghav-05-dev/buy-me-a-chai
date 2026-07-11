446544d16c758fb893a42e28035574b45f0bcbb8
raghav-05-dev <raghavp884@gmail.com>
2026-07-11 23:25:46 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-      
+    <nav className='bg-black text-white flex justify-around'>
+      <div className='logo font-bold'>Buy me a chai</div>
+      <ul className='flex justify-between gap-4'>
+        <li>home</li>
+        <li>about</li>
+        <li>projects</li>
+        <li>login</li>
+      </ul>
     </nav>
   )
 }
