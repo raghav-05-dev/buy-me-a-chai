db51a9105ccc7591dacb1bd7f8c1c16fbfd4ce0f
raghav-05-dev <raghavp884@gmail.com>
2026-07-12 19:39:48 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,38 +1,51 @@
-import NextAuth from 'next-auth';
-// import AppleProvider from 'next-auth/providers/apple'
-// import FacebookProvider from 'next-auth/providers/facebook'
-// import GoogleProvider from 'next-auth/providers/google'
-// import EmailProvider from 'next-auth/providers/email'
-import GitHubProvider from "next-auth/providers/github";
+// import NextAuth from 'next-auth';
+// // import AppleProvider from 'next-auth/providers/apple'
+// // import FacebookProvider from 'next-auth/providers/facebook'
+// // import GoogleProvider from 'next-auth/providers/google'
+// // import EmailProvider from 'next-auth/providers/email'
+// import GitHubProvider from "next-auth/providers/github";
+
+// export const authoptions = NextAuth({
+//     providers: [
+//         // OAuth authentication providers...
+//         GitHubProvider({
+//             clientId: process.env.GITHUB_ID,
+//             clientSecret: process.env.GITHUB_SECRET
+//         }),
+//         // AppleProvider({
+//         //     clientId: process.env.APPLE_ID,
+//         //     clientSecret: process.env.APPLE_SECRET
+//         // }),
+//         // FacebookProvider({
+//         //     clientId: process.env.FACEBOOK_ID,
+//         //     clientSecret: process.env.FACEBOOK_SECRET
+//         // }),
+//         // GoogleProvider({
+//         //     clientId: process.env.GOOGLE_ID,
+//         //     clientSecret: process.env.GOOGLE_SECRET
+//         // }),
+//         // Passwordless / email sign in
+//         // EmailProvider({
+//         //     server: process.env.MAIL_SERVER,
+//         //     from: 'NextAuth.js <no-reply@example.com>'
+//         // }),
+//     ],
+// })
+
+//  export { authoptions as GET, authoptions as POST}
+
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
 export const authOptions = {
     providers: [
-        // OAuth authentication providers...
-        GitHubProvider({
+        GitHub({
             clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
+            clientSecret: process.env.GITHUB_SECRET,
         }),
-        // AppleProvider({
-        //     clientId: process.env.APPLE_ID,
-        //     clientSecret: process.env.APPLE_SECRET
-        // }),
-        // FacebookProvider({
-        //     clientId: process.env.FACEBOOK_ID,
-        //     clientSecret: process.env.FACEBOOK_SECRET
-        // }),
-        // GoogleProvider({
-        //     clientId: process.env.GOOGLE_ID,
-        //     clientSecret: process.env.GOOGLE_SECRET
-        // }),
-        // Passwordless / email sign in
-        // EmailProvider({
-        //     server: process.env.MAIL_SERVER,
-        //     from: 'NextAuth.js <no-reply@example.com>'
-        // }),
     ],
+
     secret: process.env.NEXTAUTH_SECRET,
-}
+};
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export const { handlers: { GET, POST } } = NextAuth(authOptions);
