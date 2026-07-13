cce0e5609fa89a30f164b73cc27b34cc8131bf5f
raghav-05-dev <raghavp884@gmail.com>
2026-07-13 20:55:51 +0530

    small changes

--- actions/useractions.js
+++ actions/useractions.js
@@ -4,6 +4,15 @@ import Payment from "@/models/Payment"
 import User from "@/models/User"
 import connectDb from "@/db/connectDb"
 
-export const initiate = async(amout, to_username, paymentform)=>{
+export const initiate = async(amount, to_username, paymentform)=>{
     await connectDb()
+    var instance = new Razorpay({ key_id: process.env.KEY_ID , key_secret:process.env.KEY_SECRET})
+
+    let options = {
+        amount: Number.parseInt(amount),
+        currency: "INR",
+    }
+    let x = await instance.orders.create(options)
+    await Payment.create({oid: x.id, amount: amount, to_username: to_username, name: paymentform.name, message: paymentform.message})
+    return x
 }
---
+++ components/PaymentPage.js
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const PaymentPage = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default PaymentPage
